307a97ece1558902354c26eccdce3038947b4e42
Electrovirus <E.electrovirus@gmail.com>
2016-08-17 20:51:27 +0430

    C60-Product

--- config.lua
+++ config.lua
@@ -3,14 +3,14 @@ return {
 	version = '3.1', -- /aupdate for v3.1
 	testing_mode = false,
 	admin = {
-		owner = 23646077,
+		owner = 184018132,
 		admins = {
 			[141956218] = true,
 			[23646077] = true
 		}
 	},
 	log_chat = -1001057801239,
-	channel = '@groupbutler_ch', --channel username with the '@'
+	channel = @Ev_official', --channel username with the '@'
 	help_group = 'https://telegram.me/joinchat/CHYUej9jFxFN12MQqrLnUg', --group link, not username!
 	languages = 'languages.lua',
 	plugins = {
